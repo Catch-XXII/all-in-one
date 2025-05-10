22b6d18d8bda74133dfab1e25612a01d57e676a7
Cuneyd Kaya <cgultekink@gmail.com>
2025-05-10 21:56:01 +0300

    wrong api path was fixed

--- backend/app/api/v1/endpoints/hello.py
+++ backend/app/api/v1/endpoints/hello.py
@@ -3,6 +3,6 @@ from fastapi import APIRouter
 router = APIRouter()
 
 
-@router.get("/api/v1/hello")
+@router.get("/hello")
 async def hello(name: str = "world"):
     return {"message": f"Hello, {name}!"}
